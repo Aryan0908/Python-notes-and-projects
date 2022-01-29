0a992b5c68e03c34499b6eb76aef68315755cce7
Aryan0908 <68681552+Aryan0908@users.noreply.github.com>
2022-01-29 17:31:28 +0530

    Update Day 32_- Email Smtp.py

--- All-notes/Notes/Day 32_- Email Smtp.py
+++ All-notes/Notes/Day 32_- Email Smtp.py
@@ -19,4 +19,19 @@ with smtplib.SMTP("smtp.gmail.com") as connection:
     connection.sendmail(from_addr=my_email,
                         to_addrs="aryan0908@yahoo.com",
                         msg="Subject:This is how you put subject.\n\nThis is how you put body")
+    
+    # To make the message more clear and readable we use EmailMessage from email.message module
+    message = EmailMessage()
+        message["Subject"] = "Happy Birthday"
+        message["From"] = my_mail
+        message["To"] = person_email
+        message.set_content(f"Hey {person_name},\n\nHAPPY BIRTHDAY,\nHope all your birthday wishes come true."
+                            f" Let's go out and celebrate this awful day 🤢. Hope that you accept the truth.\n"
+                            f"Anyway it's your special day - get out there and celebrate")
+        
+    # Adding an image as an attachment
+        with open("Neon Drinks Birthday Greeting Poster.png", "rb") as file:
+            birthday_image = file.read()
+
+        message.add_attachment(birthday_image, maintype="image", subtype="png", filename="Birthday Poster")
 
