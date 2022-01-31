3ff1ec95b83f20598fb068934fcf5f739cf85dc2
Aryan0908 <68681552+Aryan0908@users.noreply.github.com>
2022-01-31 23:57:51 +0530

    Update Day 33_ API.py

--- All-notes/Notes/Day 33_ API.py
+++ All-notes/Notes/Day 33_ API.py
@@ -1,6 +1,6 @@
 import requests
 
-# Getting hold of the api
+# Getting hold of the api data
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response)
 
@@ -37,3 +37,47 @@ parameters = {
 }
 
 response_1 = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+
+"""
+SPLIT
+"""
+
+# Posting our data into an api
+
+pixela_endpoint = "https://pixe.la/v1/users"
+pixela_params = {
+    "token": "7859804175@Aryan.",
+    "username": "aryan0908",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+"""Posting the parameters required by the site in form of a json file"""
+response = requests.post(url=pixela_endpoint, json=pixela_params)
+"""Checking if our posting succeeded"""
+print(response.text)
+
+"""
+SPLIT
+"""
+
+# Authentication ourself with HTTP Header (if required by the api)
+"""It is more secure way so that our API keys don't get leak"""
+
+header = {
+    "Required key": "value"
+}
+
+response = requests.post(url="", json="", headers=header)
+print(response.text)
+
+"""
+SPLIT
+"""
+
+# Put: Updating the existing data
+response = requests.post(url="", json="", headers=header)
+
+# Delete: Deleting the existing data
+response = requests.post(url="", json="if parameters are required", headers=header)
+
