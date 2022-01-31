da761777561196007c99d8bcf2dcca1179067c5a
Aryan0908 <68681552+Aryan0908@users.noreply.github.com>
2022-01-31 23:44:29 +0530

    Update Day 32_- DayTime module.py

--- All-notes/Notes/Day 32_- DayTime module.py
+++ All-notes/Notes/Day 32_- DayTime module.py
@@ -16,3 +16,7 @@ print(day_of_week)
 # Setting our own date and time
 our_datetime = dt.datetime(year=2002, month=9, day=8)
 print(our_datetime)
+
+# strftime: To get a particular format o date and time we use
+"""To get the particulars of strftime visite https://www.w3schools.com/python/python_datetime.asp"""
+print(now.strftime("%Y%m%d"))
